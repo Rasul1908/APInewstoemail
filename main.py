4302d4e118c15f2d3c9e8cd65448b537f23f49d7
inter <fc-inter08@mail.ru>
2023-02-22 11:53:08 +0400

    Send email, specify language, limit, news,add subject, control category

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 import requests
-
+from emailsender import send_email
+topic='chatgpt'
 api_key='0bd9cab9007641548618130eeba010a7'
-url='https://newsapi.org/v2/everything?q=tesla&from=2023-01-19&sortBy=publishedAt&apiKey=0bd9cab9007641548618130eeba010a7'
+url='https://newsapi.org/v2/everything?' \
+    f'q={topic}&from=2023-02-19&sortBy=publishedAt&' \
+    'apiKey=0bd9cab9007641548618130eeba010a7&language=en'
 
 request=requests.get(url)
 content=request.json()
-for article in content["articles"]:
-    print(article['title'])
-    print(article['description'])
+body =''
+for article in content["articles"][:20]:
+    if article['title'] is not None and article['description'] is not None :
+
+        body="Subject: Today's news" \
+             + "\n" + body+ article['title'] \
+             + '\n' + article['description'] \
+             + '\n'\
+             + article['url'] +2* '\n'
+
+body=body.encode('utf-8')
+
+send_email(body)
+print(body)
+
+
 
